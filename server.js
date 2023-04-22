d57382632f7f8b1132d2381000ff2a17e7a63870
zyc404 <zyc404@gmail.com>
2023-04-22 17:12:30 +0800

    遗漏代码

--- server.js
+++ server.js
@@ -1,37 +1,52 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 
 import { getPttBuffer } from './common.js'
 
 const server = fastify({
-    logger: false
+  bodyLimit: 10 * 1024 * 1024, //30m文件限制
+  logger: false
 })
-
+await server.register(multipart)
 await server.register(cors, {
-    origin: '*'
+  origin: '*'
 })
 server.get('*', (request, reply) => {
   reply.send('-- Media Server --')
 })
 server.post('/audio', async (request, reply) => {
+  let result
+  if (request.headers['content-type'].includes('multipart/form-data')) {
+    const files = await request.saveRequestFiles()
+    if (files.length > 0)
+      result = await getPttBuffer(files[0].filepath)
+    else {
+      reply.send({ error: '无文件' })
+      return
+    }
+  } else {
     const body = request.body || {}
-    if (body.recordUrl || body.recordBuffer) {
-        const result = await getPttBuffer(body.recordUrl || body.recordBuffer)
-        if (!result.buffer) {
-            reply.send({error: '转码失败'})
-        } else {
-            reply.send(result)
-        }
+    if (body.recordBuffer && body.recordBuffer.type === 'Buffer') {
+      const buffer = Buffer.from(body.recordBuffer.data)
+      result = await getPttBuffer(buffer)
+    } else if (body.recordUrl) {
+      result = await getPttBuffer(body.recordUrl)
     }
+  }
+  if (!result?.buffer) {
+    reply.send({ error: '转码失败' })
+  } else {
+    reply.send(result)
+  }
 })
-
 server.listen({
-    port: 3000,
-    host: '::'
-  }, (error) => {
-    if (error) {
-      server.log.error(`server start fail: ${error}`)
-    } else {
-      server.log.info(`server listening on ${server.server.address().port}`)
-    }
-  })
+  port: 3000,
+  host: '::'
+}, (error) => {
+  if (error) {
+    server.log.error(`server start fail: ${error}`)
+  } else {
+    server.log.info(`server listening on ${server.server.address().port}`)
+  }
+})
